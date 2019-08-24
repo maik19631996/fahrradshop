26c0f5542d0a8433185b9ead92f610cf804d1905
Rubyn Angelo Stark <rastark@jagaimo.works>
2019-08-24 20:04:12 +0200

    BusinessLogic first Ideas

---
+++ businesslogic/src/main/java/businesslogic/Antrieb.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businesslogic;
+
+/**
+ *
+ * @author maik
+ */
+public interface Antrieb {
+
+    int getID();
+
+    String getName();
+
+    String getBeschreibug();
+
+    String getHersteller();
+
+    double getPreis();
+}
