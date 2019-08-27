cc9eb4390c1abec09d2e6ac8e29b511cd313c449
Rene Hellmuth <rene.hellmuth@gmail.com>
2019-08-27 18:48:26 +0200

    Rahmenfarbe hinzugefügt

--- userinterface/src/main/java/com/example/bikeshopmodular/PrimaryController.java
+++ userinterface/src/main/java/com/example/bikeshopmodular/PrimaryController.java
@@ -3,11 +3,15 @@ package com.example.bikeshopmodular;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
     private ChoiceBox choiceBoxRahmen;
+    private ChoiceBox choiceBoxRahmenFarbe;
     private ChoiceBox choiceBoxReifen;
     private ChoiceBox choiceBoxAntriebe;
+    private TextField TextFieldSchritt;
+    private TextField TextFieldGroesse;
 
     @FXML
     private void switchToSecondary() throws IOException {        
@@ -17,6 +21,12 @@ public class PrimaryController {
     private void getRahmen() {
         for(Rahmen r : App.listRahmen) {
             choiceBoxRahmen.getItems().add(r.getType());
+        }
+    }
+    @FXML
+    private void getRahmenFarbe() {
+        for(Rahmen r : App.listRahmen) {
+            choiceBoxRahmenFarbe.getItems().add(r.getFarbe());
         }
     }
     @FXML
@@ -30,5 +40,20 @@ public class PrimaryController {
         for(Antrieb a : App.listAntrieb) {
             choiceBoxAntriebe.getItems().add(a.getName());
         }
+    }
+    @FXML
+    private void getSchritt() {
+       isInt(TextFieldGroesse.getText() );
+            
+        
+    }
+    @FXML
+    private void getGroesse() {
+        isInt(TextFieldSchritt.getText());
+              
+    }
+
+    private void isInt(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
--- userinterface/src/main/java/com/example/bikeshopmodular/SecondaryController.java
+++ userinterface/src/main/java/com/example/bikeshopmodular/SecondaryController.java
@@ -9,4 +9,8 @@ public class SecondaryController {
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+    
+    
+    
+    
 }
