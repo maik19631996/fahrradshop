1a920c922d75c42896de08923afe99b237499711
Rubyn Angelo Stark <rastark@jagaimo.works>
2019-08-29 23:53:33 +0200

    2 digits after the dicimal point

--- userinterface/src/main/java/com/example/bikeshopmodular/FahrradKonfiguratorAppImpl.java
+++ userinterface/src/main/java/com/example/bikeshopmodular/FahrradKonfiguratorAppImpl.java
@@ -20,15 +20,15 @@ public class FahrradKonfiguratorAppImpl implements FahrradKonfiguratorApp {
     private String antrieb;
     private int schrittlaenge;
     private int koerpergroesse;
-    
+
     private BusinessLogic businesslogic;
     private List<String> antriebe;
     private List<String> rahmentypen;
     private List<String> rahmenfarben;
     private List<String> allReifen;
-    
-    private Map m;    
-    
+
+    private Map m;
+
     public FahrradKonfiguratorAppImpl(BusinessLogic businesslogic) {
         this.businesslogic = businesslogic;
         antriebe = this.businesslogic.getAllAntriebe();
@@ -36,66 +36,66 @@ public class FahrradKonfiguratorAppImpl implements FahrradKonfiguratorApp {
         rahmenfarben = this.businesslogic.getAllRahmenfarben();
         allReifen = this.businesslogic.getAllReifen();
     }
-    
+
     @Override
     public List<String> getRahmentypen() {
         return rahmentypen;
     }
-    
+
     @Override
     public List<String> getRahmenfarben() {
         return rahmenfarben;
     }
-    
+
     @Override
     public List<String> getAllReifen() {
         return allReifen;
     }
-    
+
     @Override
     public List<String> getAntriebe() {
         return antriebe;
     }
-    
+
     @Override
     public void setRahmentyp(String rahmentyp) {
         this.rahmentyp = rahmentyp;
     }
-    
+
     @Override
     public void setRahmenfarbe(String rahmenfarbe) {
         this.rahmenfarbe = rahmenfarbe;
     }
-    
+
     @Override
     public void setReifen(String reifen) {
         this.reifen = reifen;
     }
-    
+
     @Override
     public void setAntrieb(String antrieb) {
         this.antrieb = antrieb;
-    } 
-    
+    }
+
     @Override
     public void setSchrittlaenge(int schrittlaenge) {
         this.schrittlaenge = schrittlaenge;
     }
-    
+
     @Override
     public void setKoerpergroesse(int koerpergroesse) {
         this.koerpergroesse = koerpergroesse;
     }
-    
+
     @Override
-    public void setFahrradKonfiguration() throws RuntimeException {   
-        businesslogic.setFahrradKonfiguration(rahmentyp, rahmenfarbe, reifen, antrieb, schrittlaenge, koerpergroesse);  
+    public void setFahrradKonfiguration() throws RuntimeException {
+        businesslogic.setFahrradKonfiguration(rahmentyp, rahmenfarbe, reifen, antrieb, schrittlaenge, koerpergroesse);
     }
-    
+
     @Override
     public String getSumPriceAsString() throws RuntimeException{
         setFahrradKonfiguration();
-        m = businesslogic.getPrices();      
-        return m.get("sum").toString();
+        m = businesslogic.getPrices();
+        return String.format("%.2f", m.get("sum"));
     }
 }
